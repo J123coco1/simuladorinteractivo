4d88ac6f0c24c83b338f1118f79673a7104606cf
cocoloco <jorge-daniel2010@hotmail.com>
2023-10-13 21:09:22 -0600

    se agrego el metodo find

--- js/main.js
+++ js/main.js
@@ -1,27 +1,23 @@
-//objetos
-const tiposDeCambio = {
-    "1": { nombre: "MXN A USD", tasa: 0.058 },
-    "2": { nombre: "MXN A EUR", tasa: 0.055 },
-    "3": { nombre: "EUR A USD", tasa: 1.07 },
-    "4": { nombre: "EUR A MXN", tasa: 18.32 },
-    "5": { nombre: "USD A EUR", tasa: 0.94 },
-    "6": { nombre: "USD A MXN", tasa: 17.19 }
-};
+const tiposDeCambio = [
+    { id: "1", nombre: "MXN A USD", tasa: 0.058 },
+    { id: "2", nombre: "MXN A EUR", tasa: 0.055 },
+    { id: "3", nombre: "EUR A USD", tasa: 1.07 },
+    { id: "4", nombre: "EUR A MXN", tasa: 18.32 },
+    { id: "5", nombre: "USD A EUR", tasa: 0.94 },
+    { id: "6", nombre: "USD A MXN", tasa: 17.19 }
+];
 
-//array
 const unidadesDeMoneda = [
     { nombre: "MXN", cantidad: 0 },
     { nombre: "USD", cantidad: 0 },
     { nombre: "EUR", cantidad: 0 }
 ];
 
+function convertirDivisas(cantidad, tipoCambio) {
+    return cantidad * tipoCambio.tasa;
+}
 
-
-
-let seguir = true;
-
-while (seguir) {
-
+function realizarConversion() {
     console.log("1.- MXN A USD");
     console.log("2.- MXN A EUR");
     console.log("3.- EUR A USD");
@@ -31,30 +27,33 @@ while (seguir) {
 
     let respuesta = prompt("Selecciona el número que corresponda al tipo de cambio que deseas: (o presiona 'E' para salir): ");
 
-    if(respuesta.toLowerCase() === "e"){
-        seguir = false;
-        alert("Que tengas un excelente dia amigo :D")
-        alert("Si quieres realizar otra conversion por favor recarga la pagina.")
-        continue;
+    if (respuesta.toLowerCase() === "e") {
+        alert("Que tengas un excelente día amigo :D");
+        alert("Si quieres realizar otra conversión, por favor recarga la página.");
+        return false;
     }
-
-
 
     let unidades = parseFloat(prompt("Cuánto dinero deseas cambiar?"));
 
     if (isNaN(unidades)) {
-        alert("Por favor, ingresa una cantidad valida para realizar el cambio de divisas :D ");
+        alert("Por favor, ingresa una cantidad válida para realizar el cambio de divisas :D ");
+        return true;
     } else {
-        let tipoCambio = tiposDeCambio[respuesta];
+        let tipoCambio = tiposDeCambio.find(cambio => cambio.id === respuesta);
 
         if (tipoCambio) {
-            let cambio = unidades * tipoCambio.tasa;
+            let cambio = convertirDivisas(unidades, tipoCambio);
             alert("$" + unidades + " es igual a " + tipoCambio.nombre + ": " + cambio.toFixed(2));
-          } else {
+        } else {
             alert("El tipo de cambio que elegiste no es válido, inténtalo de nuevo :D");
         }
-    }
 
+        return true;
+    }
 }
 
-// / <script src="/js/main.js"></script>
+let seguir = true;
+
+while (seguir) {
+    seguir = realizarConversion();
+}
